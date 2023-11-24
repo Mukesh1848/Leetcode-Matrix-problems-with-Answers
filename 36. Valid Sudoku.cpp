778293043fda2713d0f310af4b17acc8b5f0d7d2
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2023-11-24 17:10:22 +0530

    Create 36. Valid Sudoku.cpp

---
+++ 36. Valid Sudoku.cpp
@@ -0,0 +1,32 @@
+// 36. Valid Sudoku
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+         unordered_set<string> seen;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char curr = board[i][j];
+                if (curr != '.') {
+                    string row = to_string(i) + "r" + curr;
+                    string col = to_string(j) + "c" + curr;
+                    string box = to_string(i/3) + to_string(j/3) + "b" + curr;
+                    // if current element present in set (return false)
+                    if (seen.count(row) || seen.count(col) || seen.count(box)) {
+                        return false;
+                    }
+                    seen.insert(row);
+                    seen.insert(col);
+                    seen.insert(box);
+                }
+            }
+        }
+        return true;
+    }
+};
+
+/*
+time complexit - O(n)
+space complexity - O(n)
+*/
